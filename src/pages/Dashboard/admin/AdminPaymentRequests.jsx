04ee815f82787a61dbd404789aadb49699d5a75f
suhanur-rohoman-sumon <sumonsuhanurrohoman@gmail.com>
2024-05-25 11:17:36 +0600

    one more file added

---
+++ src/pages/Dashboard/admin/AdminPaymentRequests.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AdminPaymentRequests = () => {
+  return (
+    <div>
+      <h1>this is admin payments</h1>
+    </div>
+  );
+};
+
+export default AdminPaymentRequests;
