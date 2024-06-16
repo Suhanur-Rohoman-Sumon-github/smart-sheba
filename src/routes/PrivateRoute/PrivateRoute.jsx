eb2b0b2717d262da2c9821ebada83021059f2844
suhanur-rohoman-sumon <sumonsuhanurrohoman@gmail.com>
2024-06-16 12:03:44 +0600

    some more change

--- src/routes/PrivateRoute/PrivateRoute.jsx
+++ src/routes/PrivateRoute/PrivateRoute.jsx
@@ -1,13 +1,14 @@
-import { Navigate, useLocation, useNavigate } from "react-router-dom";
+import {} from "react-router-dom";
 import useContexts from "../../hooks/useContexts";
-
+c;
 const PrivateRoutes = ({ children }) => {
-  const { user, loading } = useContexts();
-  const location = useLocation();
   const navigate = useNavigate();
+  const { user } = useContexts();
 
   if (user) {
     return children;
+  } else {
+    navigate("/");
   }
 };
 
